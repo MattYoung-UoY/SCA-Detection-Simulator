a4e6cc9705576d62b782548c6e35b3a4deb6a4f3
Matt Young <mjy503@york.ac.uk>
2024-05-15 16:59:53 +0100

    Another idea for a future test

---
+++ discard_proportion_plots.py
@@ -0,0 +1,4 @@
+# Depending on what estimated mean value threshold causes a detection of an error:
+    # How quickly is a detection made?
+    # What proportion of key is discarded?
+    # Can that proportion be minimised based on the value of delta_p?
